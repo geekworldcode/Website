46f489c1908c4940be3c1e0e783702bfb4a34ba1
GeekWorld <96954482+geekworldcode@users.noreply.github.com>
2023-02-19 12:38:04 +0100

    Add files via upload

---
+++ chat.php
@@ -0,0 +1,35 @@
+<?php
+$bdd = new PDO('mysql:host=localhost;dbname;charset=utf8;','geekworldlegamerz', 'Japon2008')
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="monsite.css">
+    <title>Vincent, développeur informatique</title>
+    <link rel="stylesheet" href="monsite.css">
+    <link rel="preconnect" href="https://fonts.gstatic.com">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.2.1/css/all.min.css" integrity="sha512-MV7K8+y+gLIBoVD59lQIYicR65iaqukzvf/nwasF0nqhPay5w/9lJmVM2hMDcnK1OnMGCdVK+iQrJ7lzPJQd1w==" crossorigin="anonymous" referrerpolicy="no-referrer" />
+</head>
+<body>
+    <form method="post" action="" align="center">
+        <input type="text" name="pseudo">
+        <br><br>
+        <textarea name="message"></textarea>
+        <br>
+        <input type="submit" name="valider">
+    </form>
+
+
+
+    <div class="cr-website">
+        
+        <p>Laissez-moi vous créer un site web.</p>
+        <a class="button-two" href="products.html">Voir</a>
+
+    </div>
+
+</body>
+</html>
